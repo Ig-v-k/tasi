52ed5e2c872faef1552ade475955610167f3e347
Igor Wysocki <lgorvysocky@gmail.com>
2024-03-03 22:54:59 +0300

    postgresql jdbc

---
+++ src/main/java/com/iw/jdbc/PsqlJDBC.java
@@ -0,0 +1,30 @@
+package com.iw.jdbc;
+
+import com.iw.JDBC;
+
+public final class PsqlJDBC implements JDBC {
+
+    private final String host;
+    private final int port;
+    private final String database;
+    private final String username;
+    private final String password;
+
+    public PsqlJDBC(String username, String password) {
+        this("localhost", 5432, "postgres", username, password);
+    }
+
+    public PsqlJDBC(String host, int port, String database, String username, String password) {
+        this.host = host;
+        this.port = port;
+        this.database = database;
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public String url() {
+        return String.format(
+                "jdbc:postgresql://%s:%s/%s?user=%s&password=%s", host, port, database, username, password);
+    }
+}
--- src/test/java/com/iw/JDBCTest.java
+++ src/test/java/com/iw/JDBCTest.java
@@ -1,6 +1,9 @@
 package com.iw;
 
+import com.iw.jdbc.PsqlJDBC;
 import org.junit.Test;
+
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -9,5 +12,16 @@ public final class JDBCTest {
     public void lambda() {
         final JDBC jdbc = () -> "foo";
         assertThat(jdbc.url()).isEqualTo("foo");
+    }
+
+    @Test
+    public void urlByEnvCredentials() {
+        final Map<String, String> envs = System.getenv();
+        final String username = envs.get("username");
+        final String password = envs.get("password");
+        final JDBC jdbc = new PsqlJDBC(username, password);
+        final String expected = String.format(
+                "jdbc:postgresql://localhost:5432/postgres?user=%s&password=%s", username, password);
+        assertThat(jdbc.url()).isEqualTo(expected);
     }
 }
