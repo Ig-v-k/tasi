a38f99d8882ae8598a953d5faa8c9689f8762816
Igor Wysocki <lgorvysocky@gmail.com>
2024-03-03 13:41:52 +0300

    start server + resources

--- src/main/java/com/iw/App.java
+++ src/main/java/com/iw/App.java
@@ -1,7 +1,14 @@
 package com.iw;
 
+import io.javalin.Javalin;
+import io.javalin.http.HttpStatus;
+import io.javalin.http.staticfiles.Location;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Javalin.create(cfg -> cfg.staticFiles.add("/assets/public", Location.CLASSPATH))
+                .get("/", ctx -> ctx.result("Home page"))
+                .error(HttpStatus.NOT_FOUND, ctx -> ctx.result("404. Page not found"))
+                .start(8080);
     }
 }
