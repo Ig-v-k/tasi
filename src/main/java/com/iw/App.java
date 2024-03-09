177f2d416bce29e930c8e6c408a1e076f9b9cf35
Igor Wysocki <lgorvysocky@gmail.com>
2024-03-09 19:40:46 +0300

    app start

--- src/main/java/com/iw/App.java
+++ src/main/java/com/iw/App.java
@@ -1,6 +1,6 @@
 package com.iw;
 
-import com.iw.container.SqlContainer;
+import com.iw.container.HikariContainer;
 import com.iw.jdbc.PsqlJDBC;
 import com.iw.page.HomePage;
 import io.javalin.Javalin;
@@ -11,10 +11,10 @@ public class App {
     public static void main(String[] args) {
         final String username = System.getenv("username");
         final String password = System.getenv("password");
-        final Container container = new SqlContainer(new PsqlJDBC(username, password));
+        final Container container = new HikariContainer(new PsqlJDBC(username, password));
         final Page pHome = new HomePage(container);
         Javalin.create(cfg -> cfg.staticFiles.add("/assets/public", Location.CLASSPATH))
-                .get("/", ctx -> ctx.result(pHome.render()))
+                .get("/", ctx -> ctx.html(pHome.render()))
                 .error(HttpStatus.NOT_FOUND, ctx -> ctx.result("404. Page not found"))
                 .start(8080);
     }
