c24b00850c399f6909313d794b1094621bf4e7e4
Igor Wysocki <lgorvysocky@gmail.com>
2024-03-27 23:27:02 +0300

    table

--- src/main/java/com/iw/facet/IssuesFacet.java
+++ src/main/java/com/iw/facet/IssuesFacet.java
@@ -3,13 +3,13 @@ package com.iw.facet;
 import com.iw.Facet;
 import com.iw.Issue;
 import j2html.tags.Tag;
-import j2html.tags.specialized.MainTag;
+import j2html.tags.specialized.TableTag;
 
 import java.util.List;
 
 import static j2html.TagCreator.*;
 
-public final class IssuesFacet implements Facet<MainTag> {
+public final class IssuesFacet implements Facet<TableTag> {
 
     private final List<Issue> issues;
 
@@ -18,12 +18,33 @@ public final class IssuesFacet implements Facet<MainTag> {
     }
 
     @Override
-    public Tag<MainTag> tag() {
-        return main(each(issues, (i, e) -> {
-            final String title = e.title();
-            return article(
-                    h2(a(title).withHref("/issue/" + e.id()))
-            );
-        }));
+    public Tag<TableTag> tag() {
+        return table(
+                tr(
+                        th("T"),
+                        th("Key"),
+                        th("Summary"),
+                        th("Assignee"),
+                        th("Reporter"),
+                        th("P"),
+                        th("Status"),
+                        th("Resolution"),
+                        th("Created"),
+                        th("Updated")
+                ), each(issues, (i, e) -> {
+                    final String title = e.title();
+                    return tr(
+                            td("-T-"),
+                            td("-Key-"),
+                            td(title),
+                            td("-Assignee-"),
+                            td("-Reporter-"),
+                            td("-P-"),
+                            td("-Status-"),
+                            td("-Resolution-"),
+                            td("-Created-"),
+                            td("-Updated-"));
+                })
+        );
     }
 }
